7830b5c5da8f63bfb362c04c4945dc8a087acf71
unknown <volodymyr>
2021-11-03 20:53:23 +0200

    feat: compeleted task sun-of-params

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const action = process.argv[2];
+const workData = process.argv.slice(3);
+
+if (!['add', 'multiply'].includes(action)) {
+
+  // eslint-disable-next-line
+  console.log(`Operation is not supported!`);
+  process.exit();
+}
+
+const getValue = workData.reduce((acc, el) => {
+  if (!isNaN(Number(el))) {
+    switch (action) {
+      case 'add':
+        return acc + Number(el);
+      case 'multiply':
+        return acc * Number(el);
+      default:
+        return null;
+    }
+  }
+}, 0);
+
+// eslint-disable-next-line
+console.log(getValue || `There is nothing to ${action}`);
