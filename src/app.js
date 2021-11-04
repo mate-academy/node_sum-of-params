c53e809d38d6b91bc093af131b9dbe85a2a0a2e5
unknown <volodymyr>
2021-11-05 00:00:19 +0200

    fix: add multiplication and division by zero

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const action = process.argv[2];
 const workData = process.argv.slice(3);
 
 if (!['add', 'multiply'].includes(action)) {
-
   // eslint-disable-next-line
   console.log(`Operation is not supported!`);
   process.exit();
@@ -14,14 +13,14 @@ const getValue = workData.reduce((acc, el) => {
   if (!isNaN(Number(el))) {
     switch (action) {
       case 'add':
-        return acc + Number(el);
+        return Number(acc) + Number(el);
       case 'multiply':
-        return acc * Number(el);
+        return Number(acc) * Number(el);
       default:
         return null;
     }
   }
-}, 0);
+});
 
 // eslint-disable-next-line
-console.log(getValue || `There is nothing to ${action}`);
+console.log(getValue >= 0 ? getValue : `There is nothing to ${action}`);
