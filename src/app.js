ff77f7eb8b6ed153c715915244fb4eaf6e5459be
khrystynastyslo <Ukjhsz50DDMS>
2021-11-05 14:27:50 +0200

    fixed

--- src/app.js
+++ src/app.js
@@ -4,16 +4,24 @@ const operation = process.argv[2];
 const numbers = process.argv.slice(3);
 let res;
 
-if (operation === 'add' && numbers.length > 0) {
+if (!operation) {
+  res = 'Operation is not supported!';
+} else if (operation === 'add' && numbers.length > 0) {
   res = numbers.reduce((acc, cur) => {
+    if (isNaN(Number(cur))) {
+      return acc;
+    }
+
     return Number(acc) + Number(cur);
   }, 0);
 } else if (operation === 'multiply' && numbers.length > 0) {
   res = numbers.reduce((acc, cur) => {
+    if (isNaN(Number(cur))) {
+      return acc;
+    }
+
     return Number(acc) * Number(cur);
   }, 1);
-} else if (!operation) {
-  res = 'Operation is not supported!';
 } else if (numbers.length === 0) {
   res = 'There is nothing to add (or multiply)';
 }
