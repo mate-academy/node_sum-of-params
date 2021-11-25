56eecfdcf6c7d04b8792504aead605c15225deea
PresidentComanch <ap.dissembler@gmail.com>
2021-11-26 00:25:32 +0200

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const [action, ...args] = process.argv.slice(2);
+const filteredArgs = args.map(el => Number(el)).filter(el => !isNaN(el));
+
+const phrase = 'There is nothing to ';
+
+switch (action) {
+  case 'add':
+    (filteredArgs.length > 0) ? console.log(filteredArgs.reduce((sum, a) => sum + a)) : console.log(phrase + action);
+    break;
+  case 'multiply':
+    (filteredArgs.length > 0) ? console.log(filteredArgs.reduce((sum, a) => sum * a)) : console.log(phrase + action);
+    break;
+  default:
+    console.log(`Operation is not supported!`);
+}
