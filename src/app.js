bb6773289b744424602cb7dacef17da364c60588
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2021-12-04 19:28:48 +0200

    Resolved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
 'use strict';
+
+
+sumOfParams();
+
+function sumOfParams() {
+  const dataFromTerminal = process.argv.slice(3);
+  const action = process.argv[2];
+  
+  switch (action) {
+    case 'add':
+      const result = dataFromTerminal.map(num => +num).reduce((a,b) => a + b)
+
+      if (!result) {
+        console.log('There is nothing to add');
+        break
+      }
+  
+      console.log(result)
+      break;
+
+    case 'mutiply':
+      const res = dataFromTerminal.map(num => +num).reduce((a,b) => a * b)
+
+      if (!res) {
+        console.log('There is nothing to mutiply')
+        break;
+      } 
+
+      console.log(res)
+      break;
+
+    default:
+      console.log('Operation is not supported!');
+      break;
+  }  
+}
+
