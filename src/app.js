39fd9f15b2ccb348f18e69617ab7cd995c722fa8
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2021-12-07 12:29:30 +0200

    Fixed

--- src/app.js
+++ src/app.js
@@ -1,38 +1,42 @@
 'use strict';
 
-
 sumOfParams();
 
 function sumOfParams() {
   const dataFromTerminal = process.argv.slice(3);
   const action = process.argv[2];
-  
+
   switch (action) {
     case 'add':
-      const result = dataFromTerminal.map(num => +num).reduce((a,b) => a + b)
-
-      if (!result) {
+      if (!dataFromTerminal.length) {
+        // eslint-disable-next-line no-console
         console.log('There is nothing to add');
-        break
+        break;
       }
-  
-      console.log(result)
+
+      const result = dataFromTerminal.map(num => +num).reduce((a, b) => a + b);
+
+      // eslint-disable-next-line no-console
+      console.log(result);
       break;
 
     case 'mutiply':
-      const res = dataFromTerminal.map(num => +num).reduce((a,b) => a * b)
-
-      if (!res) {
-        console.log('There is nothing to mutiply')
+      if (!dataFromTerminal.length) {
+        // eslint-disable-next-line no-console
+        console.log('There is nothing to mutiply');
         break;
-      } 
+      }
 
-      console.log(res)
+      const res = dataFromTerminal.map(num => +num).reduce((a, b) => a * b);
+
+      // eslint-disable-next-line no-console
+      console.log(res);
       break;
 
     default:
+
+      // eslint-disable-next-line no-console
       console.log('Operation is not supported!');
       break;
-  }  
+  };
 }
-
