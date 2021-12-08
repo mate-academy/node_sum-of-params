57e32744c7c0a638c656dbfeb9e026c17a226342
Bohdan P <bohdan.prymyak@izzi.software>
2021-12-08 17:07:43 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
+
+const operation = process.argv[2];
+const numbers = process.argv.slice(3).filter(item => isFinite(Number(item)));
+
+if (!['add', 'mutiply'].includes(operation)) {
+  return console.log('Operation is not supported!');
+}
+
+if (numbers.length === 0) {
+  return console.log(`There is nothing to ${operation}`);
+}
+
+const result = numbers
+  .map(Number)
+  .reduce((acc, item) => {
+    switch (operation) {
+      case 'add':
+        return acc + item;
+      case 'mutiply':
+        return acc * item;
+      default:
+        return 0;
+    }
+  }, operation === 'add' ? 0 : 1);
+
+console.log(result);
