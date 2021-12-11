66c551b1139cd14dd27d21d1e4ed15d3575ddec2
Oleg Semko <oleg.semko@zazmic.com>
2021-12-11 20:51:53 +0200

    linter

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
 'use strict';
 
-const operations = {add: 'add', multiply: 'multiply'};
+const operations = {
+  add: 'add', multiply: 'multiply',
+};
 const operation = process.argv[2];
 const args = process.argv.slice(3);
 
 if (operation === operations.add) {
-	if (args.length > 1) {
-		let sum = args.reduce((prevVal, currVal) => {
-			return +prevVal + +currVal;
-		}, 0);
-		console.log(sum);
-	} else {
-		console.log('There is nothing to add');
-	}
+  if (args.length > 1) {
+    const sum = args.reduce((prevVal, currVal) => {
+      return +prevVal + +currVal;
+    }, 0);
+
+    return sum;
+  } else {
+    return 'There is nothing to add';
+  }
 } else if (operation === operations.multiply) {
-	if (args.length > 1) {
-		let mult = args.reduce((prevVal, currVal) => {
-			return +prevVal * +currVal;
-		}, 1);
-		console.log(mult);
-	} else {
-		console.log('There is nothing to multiply');
-	}
+  if (args.length > 1) {
+    const mult = args.reduce((prevVal, currVal) => {
+      return +prevVal * +currVal;
+    }, 1);
+
+    return mult;
+  } else {
+    return 'There is nothing to multiply';
+  }
 } else {
-	console.log('Operation is not supported!');
+  return 'Operation is not supported!';
 }
