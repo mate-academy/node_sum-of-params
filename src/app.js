177727427575d63126937695ac411037c38159fb
Oleg Semko <oleg.semko@zazmic.com>
2021-12-11 20:33:08 +0200

    my solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const operations = {add: 'add', multiply: 'multiply'};
+const operation = process.argv[2];
+const args = process.argv.slice(3);
+
+if (operation === operations.add) {
+	if (args.length > 1) {
+		let sum = args.reduce((prevVal, currVal) => {
+			return +prevVal + +currVal;
+		}, 0);
+		console.log(sum);
+	} else {
+		console.log('There is nothing to add');
+	}
+} else if (operation === operations.multiply) {
+	if (args.length > 1) {
+		let mult = args.reduce((prevVal, currVal) => {
+			return +prevVal * +currVal;
+		}, 1);
+		console.log(mult);
+	} else {
+		console.log('There is nothing to multiply');
+	}
+} else {
+	console.log('Operation is not supported!');
+}
