8c7b51395d8395c2c7e2957bf5e2d225ba7d1e04
daviddohru@gmail.com <qwerty12AS12AS>
2021-12-13 23:02:46 +0300

    save changes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const start = () => {
+  const data = process.argv.slice(2);
+
+  const check = data.slice(1).every(item => !isNaN(+item));
+
+  if (check) {
+    if (data[0] === 'add') {
+      const res = data.splice(1).reduce((a, b) => (+a + +b));
+
+      // eslint-disable-next-line no-console
+      console.log(res);
+    } else if (data[0] === 'multiply') {
+      const res = data.splice(1).reduce((a, b) => (+a * +b));
+
+      // eslint-disable-next-line no-console
+      console.log(res);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Operation is not supported!');
+    }
+  } else {
+    if (data[0] === 'add') {
+      // eslint-disable-next-line no-console
+      console.log('There is nothing to add');
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('There is nothing to multiply');
+    }
+  }
+};
+
+start();
