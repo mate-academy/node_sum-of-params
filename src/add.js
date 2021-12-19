ba3424c6609442654c988bd62bdf41e419dc3713
Valerie Yaskevych <redrum.back@gmail.com>
2021-12-19 02:52:48 +0200

    solved

---
+++ src/add.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const add = (params) => params.reduce((prev, curr) => prev + curr, 0);
+
+module.exports = { add };
--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const { add } = require('./add');
+const { multiply } = require('./multiply');
+
+const primitiveCalculator = (operation, ...params) => {
+  const messages = {
+    noData: 'There\'s nothing to add',
+    unknownOperation: 'Operation is not supported!',
+    naughtyUser: 'Don\'t be naughty and use numbers only next time you call me',
+  };
+
+  if (params.length < 1) {
+    return messages.noData;
+  }
+
+  const preparedParams = params
+    .map(param => +param)
+    .filter(num => num || num === 0);
+
+  if (preparedParams.length < 1 || preparedParams.some(isNaN)) {
+    return messages.noData;
+  }
+
+  if (preparedParams.length !== params.length) {
+    console.log(messages.naughtyUser);
+  }
+
+  switch (operation) {
+    case 'add':
+      return add(preparedParams);
+    case 'multiply':
+      return multiply(preparedParams);
+    default:
+      return messages.unknownOperation;
+  }
+};
+
+console.log(
+  primitiveCalculator(process.argv[2], ...process.argv.slice(3)),
+);
