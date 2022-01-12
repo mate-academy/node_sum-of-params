4188ba714d9ab46b730385dc9f20109b49507c12
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-01-12 02:07:17 +0200

    for inspection Sum of params

--- src/app.js
+++ src/app.js
@@ -1,1 +1,69 @@
 'use strict';
+
+// import * as readline from 'node:readline';
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('enter operator and numbers: ', (inpTer) => {
+  const arr = inpTer.split(' ');
+
+  switch (arr[0]) {
+    case 'add':
+      resOperation(arr, '+');
+      break;
+    case 'mutiply':
+      resOperation(arr, '*');
+      break;
+
+    default:
+      txt();
+      break;
+  }
+
+  terminal.close();
+});
+
+function resOperation(arr, opp) {
+  const resArr = [];
+  let res = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (+arr[i]) {
+      resArr.push(+arr[i]);
+    }
+  }
+
+  if (resArr.length === 0) {
+    return `There is nothing to add`;
+  }
+
+  if (opp === '+') {
+    for (let index = 0; index < resArr.length; index++) {
+      if (+resArr[index]) {
+        res += resArr[index];
+      }
+    }
+
+    return res;
+  }
+
+  if (opp === '*') {
+    res = 1;
+
+    for (let index = 0; index < resArr.length; index++) {
+      if (+resArr[index]) {
+        res *= resArr[index];
+      }
+    }
+
+    return res;
+  }
+}
+
+function txt() {
+  return 'Operation is not supported!';
+}
