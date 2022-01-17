636eb8aed01b6f3c45f511f1ac0cf7753335e2df
Ihor Korniienko <i.korniienko@dinamicka.com>
2022-01-17 23:31:30 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const argv = process.argv.slice(2);
+
+const getResult = params => {
+  const values = params.slice(1).map(Number).filter(Boolean);
+
+  if (values.length === 0) {
+    return 'There is nothing to add (or multiply)';
+  }
+
+  switch (params[0]) {
+    case 'add':
+      return add(values);
+    case 'multiply':
+      return multiply(values);
+    default:
+      return 'Operation is not supported!';
+  }
+};
+
+function add(params) {
+  return params.reduce((acc, num) => {
+    return acc + parseInt(num);
+  }, 0);
+}
+
+function multiply(params) {
+  return params.reduce((acc, num) => {
+    return acc * parseInt(num);
+  });
+}
+
+getResult(argv);
