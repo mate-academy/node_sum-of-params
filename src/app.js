896f02bd1843890fe866aafea50c4b415372103a
Andrii Suprun <86914377+andrewSuprun@users.noreply.github.com>
2022-01-25 22:31:52 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,29 +28,3 @@ if (command === 'add') {
 if (command === 'multiply') {
   console.log(params.reduce(commands.multiply));
 }
-
-// const operation = process.argv[2];
-// const nums = process.argv
-//   .slice(3)
-//   .filter((item) => !isNaN(Number(item)))
-//   .map((item) => Number(item));
-
-// const supportedOperations = ["add", "multiply"];
-
-// const getResult = () => {
-//   if (!supportedOperations.includes(operation)) {
-//     return "Operation is not supported!";
-//   }
-
-//   if (nums.length < 1) {
-//     return `There is nothing to ${operation}`;
-//   }
-
-//   if (operation === "add") {
-//     return nums.reduce((prev, current) => prev + current);
-//   }
-
-//   return nums.reduce((prev, current) => prev * current);
-// };
-
-// console.log(getResult());
