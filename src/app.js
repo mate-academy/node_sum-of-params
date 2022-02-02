5bf95a8b4f34333e247fbaeabbb4e5358259bf03
Nastya <anastasia.zasiadovk@ivvysoft.com>
2022-02-02 17:45:45 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+this.add = (a, b) => +a + +b;
+this.multiply = (a, b) => +a * +b;
+
+function reducer(array, callback) {
+  return array.reduce(callback);
+}
+
+const action = process.argv[2];
+const numbers = process.argv
+  .slice(3)
+  .filter(item => !isNaN(+item));
+let result = '';
+
+if (!numbers.length) {
+  result = `There is nothing to ${action}`;
+} else if (action !== 'add' && action !== 'multiply') {
+  result = 'Operation is not supported!';
+} else {
+  result = reducer(numbers, this[action]);
+}
+
+console.log(result);
