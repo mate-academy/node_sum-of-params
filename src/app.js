bbde0a1b4f34235f5e734a794696a25c94dbdf79
Dmytro  Mykoliv <dmytro.mykoliv@indeema.com>
2022-02-13 00:23:17 +0200

    initial solution

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 'use strict';
 
-const params = process.argv.slice(2);
+const operation = process.argv.slice(2)[0];
+const inputParams = process.argv.slice(3);
+
+const calculate = (action, params) => {
+  const result = params
+    .filter(el => typeof +el === 'number')
+    .map(el => Number(el));
+
+  if (!result.length && action) {
+    return `There is nothing to ${action}`;
+  }
+
+  switch (action) {
+    case 'add':
+      return result.reduce((a, b) => a + b, 0);
+    case 'multiply':
+      return result.reduce((a, b) => a * b, 1);
+    default:
+      return 'Operation is not supported!';
+  }
+};
 
 // eslint-disable-next-line
-console.log(params);
+console.log(calculate(operation, inputParams));
