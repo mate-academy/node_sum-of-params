90ee90dbbb025399f918756eddf45e9a6651693c
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-02-20 17:22:42 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+if (process.argv[3] === undefined) {
+  return 'There is nothing to add';
+}
+
+const numbers = process.argv.slice(3);
+
+if (process.argv[2] === 'add') {
+  return numbers.reduce((a, b) => +a + +b);
+} else if (process.argv[2] === 'multiply') {
+  return numbers.reduce((a, b) => +a * +b);
+} else {
+  return 'Operation is not supported!';
+}
