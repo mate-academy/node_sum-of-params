cae016e3a5856cf7e445e235735cf4a213e4ffc4
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-04-01 20:42:51 +0300

    Finish hw.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+switch (process.argv[2]) {
+  case 'add':
+    add(process.argv.slice(3));
+    break;
+  case 'multiply':
+    multiply(process.argv.slice(3));
+    break;
+  default:
+    console.log('Operation is not supported!');
+}
+
+function add(args) {
+  console.log(args.reduce((accum, value) => {
+    if (isNaN(+value)) {
+      return +accum;
+    }
+
+    return +accum + +value;
+  }, 0));
+}
+
+function multiply(args) {
+  console.log(args.reduce((accum, value) => {
+    if (isNaN(+value)) {
+      return +accum;
+    }
+
+    return +accum * +value;
+  }, 1));
+}
