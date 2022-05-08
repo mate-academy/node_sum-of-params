eab0ff291b7cab8646815495f5b497ea47f4a434
Bohdan Maksymiuk <bohdan.maksymiuk@ukr.net>
2022-05-08 23:04:37 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
+
+const params = process.argv.slice(2);
+let result;
+let isNumericValues = false;
+let isOperationSupported = true;
+
+switch (params[0]) {
+  case 'add':
+    result = 0;
+
+    for (let i = 1; i < params.length; i++) {
+      if (!isNaN(+params[i])) {
+        isNumericValues = true;
+        result += +params[i];
+      }
+    }
+    break;
+  case 'multiply':
+    result = 1;
+
+    for (let i = 1; i < params.length; i++) {
+      if (!isNaN(+params[i])) {
+        isNumericValues = true;
+        result *= +params[i];
+      }
+    }
+    break;
+  default:
+    console.log('Operation is not supported');
+    isOperationSupported = false;
+}
+
+if (isNumericValues) {
+  console.log(result);
+} else if (isOperationSupported) {
+  console.log('There is nothing to add');
+}
