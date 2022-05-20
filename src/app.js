755e532295a644cf23cac564a0d4b28764dd7f05
Taras Pyda <tapac1991@icloud.com>
2022-05-20 19:30:38 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
+/* eslint-disable no-console */
 'use strict';
+
+const [, , operation, ...values] = process.argv;
+const numbers = values.filter(value => !isNaN(value));
+
+let result = `There is nothing to ${operation}`;
+
+switch (operation) {
+  case 'add':
+    if (!numbers.length) {
+      break;
+    };
+
+    result = numbers.reduce((sum, number) => (+sum) + (+number));
+    break;
+
+  case 'multiply':
+    if (!numbers.length) {
+      break;
+    };
+
+    result = numbers.reduce((sum, number) => (+sum) * (+number));
+    break;
+
+  default:
+    result = 'Operation is not supported!';
+};
+
+console.log(result);
