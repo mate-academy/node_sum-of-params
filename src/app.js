2315ee948c67f291421176c7fb8e25ba37c3edcc
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-05-30 19:00:36 +0300

    changed

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 'use strict';
 
-if (process.argv[3] === undefined) {
-  return 'There is nothing to add';
+if (process.argv[2] !== 'add' && process.argv[2] !== 'multiply') {
+  return 'Operation is not supported!';
 }
 
-const numbers = process.argv.slice(3);
+const result = process.argv.slice(3).map((x) => +x);
 
-if (process.argv[2] === 'add') {
-  return numbers.reduce((a, b) => +a + +b);
-} else if (process.argv[2] === 'multiply') {
-  return numbers.reduce((a, b) => +a * +b);
-} else {
-  return 'Operation is not supported!';
+for (let i = 0; i < result.length; i++) {
+  if (isNaN(result[i])) {
+    return `There is nothing to ${process.argv[2]}`;
+  }
+}
+
+switch (process.argv[2]) {
+  case 'add':
+    return result.reduce((a, b) => a + b);
+  case 'multiply':
+    return result.reduce((a, b) => a * b);
+  default:
+    break;
 }
