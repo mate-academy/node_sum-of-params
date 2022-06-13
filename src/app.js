1f2faddaa38e2dc6017ceed252d3a2fb8cdc9b34
Dima Permiakov <permiakov.d.s@gmail.com>
2022-06-13 12:34:15 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const [, , action, ...items] = process.argv;
+
+let result = '';
+
+function sorted(number, operation) {
+  let buff = operation === 'multiply' ? 1 : 0;
+
+  number.map(elem => {
+    if (isNaN(+elem)) {
+      return;
+    }
+
+    switch (operation) {
+      case 'add':
+        buff += +elem;
+        break;
+      case 'multiply':
+        buff = buff * +elem;
+        break;
+      default:
+        buff = 'Operation is not supported!';
+    }
+  });
+
+  if (number.length === 0) {
+    buff = `There is nothing to ${action}`;
+  }
+
+  return buff;
+}
+
+result = sorted(items, action);
+
+// eslint-disable-next-line no-console
+console.log(result);
