a2f05b04e87bc1eb08b708bde612f58dd6518276
Misha Kravtsov <aniahucm@gmail.com>
2022-06-27 20:45:30 +0300

    solved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
+/* eslint-disable no-console */
 'use strict';
+
+const ACTIONS = {
+  ADD: 'add',
+  MULTIPLY: 'multiply',
+};
+const params = process.argv.slice(2);
+let numbersIsPresent = false;
+let temp;
+
+switch (params[0]) {
+  case ACTIONS.ADD:
+    temp = 0;
+
+    for (let i = 1; i < params.length; i++) {
+      if (!isNaN(params[i])) {
+        numbersIsPresent = true;
+        temp += parseInt(params[i]);
+      }
+    }
+
+    if (numbersIsPresent) {
+      console.log(temp);
+    } else {
+      console.log('There is nothing to add');
+    }
+    break;
+
+  case ACTIONS.MULTIPLY:
+    temp = 1;
+
+    for (let i = 1; i < params.length; i++) {
+      if (!isNaN(params[i])) {
+        numbersIsPresent = true;
+        temp *= parseInt(params[i]);
+      }
+    }
+
+    if (numbersIsPresent) {
+      console.log(temp);
+    } else {
+      console.log('There is nothing to add');
+    }
+    break;
+
+  default:
+    console.log('Operation is not supported!');
+    break;
+}
