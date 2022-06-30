3bc917f447282516df09258d5b5f026ab0b82f9e
Nazar Ostafiichuk <90191817+Ostafii4uk@users.noreply.github.com>
2022-06-30 18:41:59 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
+
+const terminal = (process.argv).slice(2);
+
+if (terminal[0] === 'add') {
+  terminal.splice(0, 1);
+
+  const prepareArray = terminal.map(el => Number(el));
+
+  console.log(prepareArray.reduce((arg, acc) => arg + acc));
+} else if (terminal[0] === 'multiply') {
+  terminal.splice(0, 1);
+
+  const prepareArray = terminal.map(el => Number(el));
+
+  console.log(prepareArray.reduce((arg, acc) => arg * acc));
+} else {
+  console.log('Operation is not supported!');
+}
