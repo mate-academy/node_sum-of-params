6a13fce7149078155f7a115d8d57d99bde7c0bab
Nazar Ostafiichuk <90191817+Ostafii4uk@users.noreply.github.com>
2022-07-06 13:15:16 +0300

    Fixed after review

--- src/app.js
+++ src/app.js
@@ -3,18 +3,26 @@
 
 const terminal = (process.argv).slice(2);
 
-if (terminal[0] === 'add') {
-  terminal.splice(0, 1);
+const prepareArray = terminal.slice(1).map(el => Number(el));
 
-  const prepareArray = terminal.map(el => Number(el));
+for (let i = 0; i < prepareArray.length; i++) {
+  if (isNaN(prepareArray[i]) === true) {
+    prepareArray.splice(i, 1);
+  }
+}
 
-  console.log(prepareArray.reduce((arg, acc) => arg + acc));
-} else if (terminal[0] === 'multiply') {
-  terminal.splice(0, 1);
-
-  const prepareArray = terminal.map(el => Number(el));
-
-  console.log(prepareArray.reduce((arg, acc) => arg * acc));
+if (!prepareArray.length) {
+  console.log('There is nothing to add (or multiply)');
 } else {
-  console.log('Operation is not supported!');
+  if (terminal[0] === 'add') {
+    terminal.splice(0, 1);
+
+    console.log(prepareArray.reduce((arg, acc) => arg + acc));
+  } else if (terminal[0] === 'multiply') {
+    terminal.splice(0, 1);
+
+    console.log(prepareArray.reduce((arg, acc) => arg * acc));
+  } else {
+    console.log('Operation is not supported!');
+  }
 }
