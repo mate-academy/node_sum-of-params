cfcaecfc6c1b61e36b153fb689537db758904205
Nazar Ostafiichuk <90191817+Ostafii4uk@users.noreply.github.com>
2022-07-06 13:22:15 +0300

    Fixed preparedArray

--- src/app.js
+++ src/app.js
@@ -3,25 +3,25 @@
 
 const terminal = (process.argv).slice(2);
 
-const prepareArray = terminal.slice(1).map(el => Number(el));
+const preparedArray = terminal.slice(1).map(el => Number(el));
 
-for (let i = 0; i < prepareArray.length; i++) {
-  if (isNaN(prepareArray[i]) === true) {
-    prepareArray.splice(i, 1);
+for (let i = preparedArray.length; i >= 0; i--) {
+  if (isNaN(preparedArray[i]) === true) {
+    preparedArray.splice(i, 1);
   }
 }
 
-if (!prepareArray.length) {
+if (!preparedArray.length) {
   console.log('There is nothing to add (or multiply)');
 } else {
   if (terminal[0] === 'add') {
     terminal.splice(0, 1);
 
-    console.log(prepareArray.reduce((arg, acc) => arg + acc));
+    console.log(preparedArray.reduce((arg, acc) => arg + acc));
   } else if (terminal[0] === 'multiply') {
     terminal.splice(0, 1);
 
-    console.log(prepareArray.reduce((arg, acc) => arg * acc));
+    console.log(preparedArray.reduce((arg, acc) => arg * acc));
   } else {
     console.log('Operation is not supported!');
   }
