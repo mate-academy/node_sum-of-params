6afe0d73bd616820c4269eb7de3fb0c842562711
Vladyslav <vladyslav3177@gmail.com>
2022-07-13 21:36:50 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const [operation, ...args] = process.argv.slice(2);
+
+const numbers = args.filter(num => +num);
+
+if (numbers.length === 0) {
+  console.log(`There is nothing to ${operation}`);
+}
+
+function operations() {
+  if (operation === 'add') {
+    const sum = numbers.reduce(
+      (initialValue, currentValue) => +initialValue + +currentValue);
+
+    console.log(sum);
+  } else if (operation === 'multiply') {
+    const multiply = numbers.reduce(
+      (initialValue, currentValue) => +initialValue * +currentValue);
+
+    console.log(multiply);
+  } else {
+    console.log('Operation is not supported!');
+  }
+}
+
+operations();
