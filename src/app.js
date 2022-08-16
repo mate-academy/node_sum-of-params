5c4980bc33671296097deba117c93c4dbdceb8bf
Illia Vodonis <illavodonis@gmail.com>
2022-08-16 23:07:04 +0300

    sloved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
+
+const [, , operator, ...num] = process.argv;
+
+const charsIsNumber = num.some(digit => typeof Number(digit) === 'number');
+let result;
+
+switch (operator) {
+  case 'multiply':
+
+    if (charsIsNumber) {
+      result = num.reduce((res, item) => {
+        if (typeof Number(item) === 'number') {
+          return res * item;
+        }
+      });
+    } else {
+      result = 'There is nothing to multiply';
+    }
+    break;
+
+  case 'add':
+    if (charsIsNumber) {
+      result = num.reduce((res, item) => {
+        if (typeof Number(item) === 'number') {
+          return res + (+item);
+        }
+      }, +0);
+    } else {
+      result = 'There is nothing to add';
+    }
+    break;
+
+  default:
+    result = 'Operation is not supported!';
+}
+
+console.log(result);
