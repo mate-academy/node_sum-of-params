554d21b472882cb50d4888ce70371e3519a8b721
Roman Kovalchuk <roma220702@gmail.com>
2022-09-09 15:12:48 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const terminalFunction = (mass) => {
+  const [operation, ...array] = mass.slice(2);
+  let result = 0;
+
+  switch (operation) {
+    case ('add'):
+      for (const item of array) {
+        result += +item;
+      }
+      break;
+    case ('multiply'):
+      result = 1;
+
+      for (const item of array) {
+        result *= +item;
+      }
+      break;
+    default:
+      return 'Operation is not supported!';
+  }
+
+  return result;
+};
+
+terminalFunction(process.argv);
