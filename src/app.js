4d1faec2bf55eaa9b675e5b5a29d821da2c394fa
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-14 15:54:33 +0300

    Task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// eslint-disable-next-line no-console
+const log = console.log;
+
+const [operation, ...operands] = process.argv.slice(2);
+const operandsPrepared = operands
+  .map(num => isNaN(+num) ? null : +num)
+  .filter(num => num !== null);
+
+if (!operandsPrepared.length) {
+  log(`There is nothing to ${operation}`);
+
+  process.exit();
+}
+
+switch (operation) {
+  case 'add': {
+    const operationResult = operandsPrepared.reduce((sum, o) => sum + o, 0);
+
+    log(operationResult);
+    break;
+  }
+
+  case 'multiply': {
+    const operationResult = operandsPrepared.reduce((mult, o) => mult * o, 1);
+
+    log(operationResult);
+    break;
+  }
+
+  default:
+    log('Operation is not supported!');
+}
