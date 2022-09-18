1af3bc5acd18b6440f11907977a60a60d9312b72
Sasha Yatsykovych <olexandr.y@mate.academy>
2022-09-18 14:54:08 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
+
+function getOperationCallback(desiredOperation) {
+  switch (desiredOperation) {
+    case 'add':
+      return (accumulator, number) => (accumulator + number);
+
+    case 'multiply':
+      return (accumulator, number) => (accumulator * number);
+
+    default:
+      console.error('Operation is not supported!');
+      process.exit(0);
+  }
+}
+
+const operation = process.argv[2];
+
+const operationCallback = getOperationCallback(operation);
+
+const numericValues = [];
+
+process.argv.slice(3).forEach((arg) => {
+  const convertedValue = Number(arg);
+
+  if (typeof convertedValue === 'number' && !Number.isNaN(convertedValue)) {
+    numericValues.push(convertedValue);
+  }
+});
+
+if (numericValues.length === 0) {
+  console.log(`There is nothing to ${operation}`);
+
+  process.exit(0);
+}
+
+const operationResult = numericValues.reduce(operationCallback);
+
+console.log('The operation result: ', operationResult);
