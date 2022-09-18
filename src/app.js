d9c68ecbcaae477cd61b5d7d64bf47c5deffaca1
Sasha Yatsykovych <olexandr.y@mate.academy>
2022-09-18 14:39:55 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const { name, lastname, age } = minimist(process.argv.slice(2));
+
+if (!name && !lastname && !age) {
+  throw new Error('Not all credentials were set');
+}
+
+// eslint-disable-next-line no-console
+console.log(`${name} ${lastname} is ${age}`);
