f2229b657cc781f4d5af22e2ab930c60ade2f8fb
Pavlo <Pmoroz2004@gmail.com>
2022-09-18 20:44:20 +0300

    my solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const { _: [act, ...num] } = minimist(process.argv.slice(2));
+
+const isNumber = (data) => {
+  return typeof data === 'number';
+};
+
+const calc = (action, numbers) => {
+  if (action !== 'add' && action !== 'multiply') {
+    return 'Operation is not supported!';
+  }
+
+  if (numbers.length === 0) {
+    return `There is nothing to ${action}`;
+  }
+
+  return numbers.reduce((prev, curr) => {
+    if (!isNumber(prev)) {
+      return curr;
+    }
+
+    if (!isNumber(curr)) {
+      return prev;
+    }
+
+    if (action === 'add') {
+      return prev + curr;
+    }
+
+    if (action === 'multiply') {
+      return prev * curr;
+    }
+  }, action === 'add' ? 0 : 1);
+};
+
+// eslint-disable-next-line no-console
+console.log(calc(act, num));
