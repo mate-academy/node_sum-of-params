07ca758a6e5108741ebf009a08af436952ca8b70
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-21 14:38:35 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+/* eslint-disable no-console */
+const numbers = '0123456789';
+const option = process.argv.slice(2);
+const args = process.argv.slice(3);
+
+switch (option[0]) {
+  case 'sum':
+    args.length
+      ? console.log(args.map(el => numbers.includes(el) ? +el : false)
+        .reduce((el, acc) => acc + el, 0))
+      : console.log('There is nothing to add');
+    break;
+  case 'multiply':
+    args.length
+      ? console.log([...args].map(el => numbers.includes(el) ? +el : 1)
+        .reduce((el, acc) => acc * el, 1))
+      : console.log('There is nothing to multiply');
+    break;
+  default:
+    console.log('Operation is not supported!');
+}
