b3ce6f08814ec81691ee6582ae67c5b686a1824c
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-21 14:41:54 +0300

    fixed throw messages

--- src/app.js
+++ src/app.js
@@ -3,19 +3,19 @@
 /* eslint-disable no-console */
 const numbers = '0123456789';
 const option = process.argv.slice(2);
-const args = process.argv.slice(3);
+const args = process.argv.slice(3).filter(el => numbers.includes(el));
 
 switch (option[0]) {
   case 'sum':
     args.length
-      ? console.log(args.map(el => numbers.includes(el) ? +el : false)
-        .reduce((el, acc) => acc + el, 0))
+      ? console.log(args.map(el => +el)
+        .reduce((el, acc) => acc + el, 0), args)
       : console.log('There is nothing to add');
     break;
   case 'multiply':
     args.length
-      ? console.log([...args].map(el => numbers.includes(el) ? +el : 1)
-        .reduce((el, acc) => acc * el, 1))
+      ? console.log([...args].map(el => +el)
+        .reduce((el, acc) => acc * el, 1), args)
       : console.log('There is nothing to multiply');
     break;
   default:
