f2236066e61f4ec663c7ae6ae6452c6e2da6ccaa
[Kate-Khon] <[katerynakhon@gmail.com]>
2022-09-22 22:02:45 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const arrayOfArgs = process.argv.slice(2);
+const action = arrayOfArgs.splice(0, 1)[0];
+const arrayOfNumbers = arrayOfArgs.filter(element => +element);
+
+const getResult = () => {
+  if (action !== 'add' && action !== 'multiply') {
+    return 'Operation is not supported!';
+  }
+
+  if (arrayOfNumbers.length === 0) {
+    return `There is nothing to ${action}`;
+  }
+
+  return action === 'add'
+    ? arrayOfNumbers.reduce((prev, number) => prev + +number, 0)
+    : arrayOfNumbers.reduce((prev, number) => prev * +number, 1)
+}
+
+console.log(getResult());
