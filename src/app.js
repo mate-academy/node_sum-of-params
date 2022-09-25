028979ee6cf27ceb8ddea8476e9e8f49286e8067
sergii2705 <maniurov_sv@ukr.net>
2022-09-25 20:39:49 +0300

    Solution ver 1.0.0

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+/* eslint-disable no-console */
+
+const [action, values] = [process.argv[2], process.argv
+  .slice(3)
+  .reduce((vals, val) => Number.isFinite(+val) ? [...vals, +val] : vals, [])];
+
+switch (action) {
+  case 'add':
+    if (values.length) {
+      console.log(values.reduce((resultOfAdd, val) => resultOfAdd + val));
+    } else {
+      console.log(`There is nothing to add`);
+    }
+    break;
+  case 'multiply':
+    if (values.length) {
+      console.log(values.reduce((resultOfMult, val) => resultOfMult * val));
+    } else {
+      console.log(`There is nothing to multiply`);
+    }
+    break;
+  default:
+    console.log(`Operation is not supported!`);
+}
