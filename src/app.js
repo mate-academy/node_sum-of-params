7e8e7e697ef35c5a85e3568500442e67be569dbd
Dina Samoilova <s13.dina@gmail.com>
2022-09-26 16:24:40 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
+
+const arrOfParams = process.argv.slice(2);
+const operation = arrOfParams[0];
+const args = arrOfParams
+  .slice(1)
+  .map((el) => +el)
+  .filter((el) => isNaN(el) === false);
+
+switch (operation) {
+  case 'add': {
+    if (args.length === 0) {
+      console.log('There is nothing to add');
+    } else {
+      console.log(args.reduce((sum, el) => sum + el, 0));
+    }
+
+    break;
+  }
+
+  case 'multiply': {
+    if (args.length === 0) {
+      console.log('There is nothing to multiply');
+    } else {
+      console.log(args.reduce((sum, el) => sum * el, 1));
+    }
+
+    break;
+  }
+  default:
+    console.log('Operation is not supported!');
+    break;
+}
