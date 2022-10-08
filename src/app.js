8050ca7b2f38d41f72788beaf5cb39a4e175546c
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-08 13:41:46 +0300

    Task complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
 'use strict';
+
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const char = process.argv[2];
+const numbers = process.argv.slice(3);
+const arrOfNumbers = numbers.filter(par => Number(par))
+  .map(numb => Number(numb));
+
+
+switch (char) {
+  case 'add':
+    if (arrOfNumbers.length !== 0) {
+      console.log(
+        arrOfNumbers.reduce((acc, x) => acc + x, 0)
+      );
+      break;
+    }
+    console.log('There is nothing to add')
+    break;
+  case 'multiply':
+    if (arrOfNumbers.length !== 0) {
+      console.log(
+        arrOfNumbers.reduce((acc, x) => acc * x, 1)
+      );
+      break;
+    }
+    console.log('There is nothing to multiply');
+    break
+  default:
+    console.log('Operation is not supported!');
+};
+
+rl.close();
