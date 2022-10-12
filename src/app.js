0ff16d2cbf4965c0de5a61efdc3ac7ccf276f73d
Anton Liada <liadaantom@gmail.com>
2022-10-12 16:51:13 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
+
+let result;
+const operation = process.argv[2];
+const restValues = process.argv.slice(3);
+const numbers = restValues
+  .map(value => Number(value))
+  .filter(num => num);
+
+if (numbers.length) {
+  switch (operation) {
+    case 'add':
+      result = numbers.reduce((a, b) => a + b, 0);
+      console.log(result);
+      break;
+
+    case 'multiply':
+      result = numbers.reduce((a, b) => a * b, 1);
+      console.log(result);
+      break;
+
+    default:
+      console.log('Operation is not supported!');
+  }
+} else {
+  console.log(`There is nothing to ${operation}`);
+}
