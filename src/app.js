d1b0683361e84f1093f0dfad419f46c237df0232
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-10-12 16:52:04 +0300

    rename variable

--- src/app.js
+++ src/app.js
@@ -24,22 +24,22 @@ function add(args) {
 const inputElements = process.argv.slice(2);
 const allowedOperations = ['add', 'multiply'];
 
-let opperation = '';
+let operation = '';
 const numbers = [];
 
 inputElements.forEach((element) => {
   if (+element) {
     numbers.push(+element);
   } else if (allowedOperations.includes(element)) {
-    opperation = element;
+    operation = element;
   }
 });
 
 if (numbers.length === 0) {
-  console.log(`There is nothing to ${opperation}`);
-} else if (opperation === 'add') {
+  console.log(`There is nothing to ${operation}`);
+} else if (operation === 'add') {
   console.log(add(numbers));
-} else if (opperation === 'multiply') {
+} else if (operation === 'multiply') {
   console.log(multiply(numbers));
 } else {
   console.log('Operation is not supported!');
