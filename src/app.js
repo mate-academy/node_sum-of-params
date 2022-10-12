8e35fbba297bff5dd612c892af97656493611433
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-10-12 14:40:37 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 'use strict';
+
+const numbers = process.argv.slice(3).filter((n) => +n);
+const operation = process.argv[2];
+
+if (!numbers.length) {
+  console.log(`There is nothing to ${operation}`);
+} else {
+  if (operation === 'add') {
+    console.log(numbers.reduce((number, total) => number + total, 0));
+  } else if (operation === 'multiply') {
+    console.log(numbers.reduce((number, total) => number * total, 1));
+  } else {
+    console.log('Operation is not supported!');
+  }
+}
