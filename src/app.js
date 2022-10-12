433bb614d62b0f5413ae9349d247198e1ba107bb
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-10-12 16:12:31 +0300

    sum of params was implemented

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
+
+function sumOfParams() {
+  const operation = process.argv[2];
+  const values = process.argv.slice(3);
+
+  let result = 1;
+
+  const filtredValues = values
+    .map(element => Number(element))
+    .filter(element => !isNaN(element));
+
+  if (filtredValues.length === 0) {
+    return `There is nothing to ${operation}`;
+  }
+
+  switch (operation) {
+    case 'add':
+      filtredValues.forEach(num => {
+        result += +num;
+      });
+      result--;
+      break;
+
+    case 'multiply':
+      filtredValues.forEach(num => {
+        result *= +num;
+      });
+
+      break;
+
+    default: return 'Operation is not supported!';
+  }
+
+  return result;
+}
+
+console.log(sumOfParams());
