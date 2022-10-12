68427d9a377f54333ee55c2abb86d1249b3d3883
Synytsyn Denys <yungdena54@gmail.com>
2022-10-12 10:29:43 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
+
+const operation = String(process.argv.splice(2, 1));
+const nums = process.argv.slice(2).map(el => !isNaN(el)
+  ? Number(el)
+  : null);
+
+if (nums.length === 0) {
+  console.log(`There is nothing to ${operation}`);
+}
+
+switch (operation) {
+  case 'add':
+    console.log(nums.reduce((a, b) => a + b));
+    break;
+
+  case 'multiply':
+    console.log(nums.reduce((a, b) => a * b));
+    break;
+
+  default:
+    console.log('Operation is not supported!');
+}
