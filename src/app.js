73f38d439c4dd8105d868f7d49a8595982e09aef
Nazar Kulchevych <102380127+Kulchevych@users.noreply.github.com>
2022-10-12 17:14:08 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function calculate() {
+  const argv = process.argv.slice(2);
+
+  const operation = argv[0];
+  const nums = argv
+    .slice(1)
+    .filter(arg => isFinite(arg))
+    .map(Number);
+
+  if (operation !== 'add' && operation !== 'multiply') {
+    return ('Operation is not supported!');
+  }
+
+  if (nums.length === 0) {
+    return `There is nothing to ${operation}`;
+  }
+
+  switch (operation) {
+    case 'add':
+      return nums.reduce((accum, num) => accum + num, 0);
+
+    case 'multiply':
+      return nums.reduce((accum, num) => accum * num, 1);
+  }
+}
+
+// eslint-disable-next-line no-console
+console.log(calculate());
