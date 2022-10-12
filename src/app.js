f85df6a12aed0cc17f1d661ba3fe2a0b392616d9
Nikita Havryliuk <chinter.games1@gmail.com>
2022-10-12 12:35:14 +0300

    some fixes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@
 
 const [operation, ...args] = process.argv.slice(2);
 
-const argsFilter = args.filter(num => !isNaN(Number(num)));
+const argsFilter = args
+  .map(el => Number(el))
+  .filter(num => !isNaN(Number(num)));
 
 if (argsFilter.length === 0) {
   console.log(`There is nothing to ${operation}`);
@@ -13,11 +15,11 @@ let operationResult;
 
 switch (operation) {
   case 'add':
-    operationResult = argsFilter.reduce((a, b) => Number(a) + Number(b), 0);
+    operationResult = argsFilter.reduce((a, b) => a + b, 0);
     break;
 
   case 'multiply':
-    operationResult = argsFilter.reduce((a, b) => Number(a) * Number(b), 1);
+    operationResult = argsFilter.reduce((a, b) => a * b, 1);
     break;
 
   default:
