f911174da6152eb746703ee991ca18af9fa0f5d4
Anatolii Vlasiuk <vlasyukalid@gmail.com>
2022-10-12 17:06:14 +0300

    Done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
+/* eslint-disable no-console */
 'use strict';
+
+const argArray = process.argv.slice(2);
+const add = (a, x) => {
+  return +a + (+x);
+};
+
+const multiply = (a, x) => {
+  return a * x;
+};
+
+function result(arr, todo, actionText) {
+  arr.shift();
+
+  const onlyNumber = arr.filter(item => +item);
+
+  if (onlyNumber.length) {
+    console.log(onlyNumber.reduce(todo));
+  } else {
+    console.log(`There is nothing to ${actionText}`);
+  }
+}
+
+switch (argArray[0]) {
+  case 'add':
+    result(argArray, add, argArray[0]);
+    break;
+  case 'multiply':
+    result(argArray, multiply, argArray[0]);
+    break;
+
+  default:
+    console.log('Operation is not supported!');
+    break;
+}
