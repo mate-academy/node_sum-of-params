8539fae5592a79fe42b93ea68ad014ebfb9af01c
Yevhen Nikolaienko <illnino380@gmail.com>
2022-10-13 21:16:05 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
+/* eslint-disable no-console */
 'use strict';
+
+const method = process.argv.slice(2, 3).join(',');
+
+const params = process.argv.slice(3).filter(param => {
+  if (param === '0') {
+    return '0';
+  }
+
+  return Number(param);
+});
+
+const add = (args) => {
+  if (!params.length) {
+    console.log('There is nothing to add');
+  } else {
+    const total = args.reduce((acc, num) => Number(acc) + Number(num), 0);
+
+    console.log(total);
+  }
+};
+
+const multiply = (args) => {
+  if (!params.length) {
+    console.log('There is nothing to multiply');
+  } else {
+    const total = args.reduce((acc, num) => acc * num, 1);
+
+    console.log(total);
+  }
+};
+
+switch (method) {
+  case 'add':
+    add(params);
+    break;
+
+  case 'multiply':
+    multiply(params);
+    break;
+
+  default:
+    console.log('Operation is not supported!');
+    break;
+}
