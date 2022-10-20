5bc83588bc04ab7baf5eebe1999f74fc56909e4e
Kamal Tufekcic <kamal.tufekcic@gmail.com>
2022-10-20 14:38:40 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const operation = process.argv[2];
+const numbers = process.argv.slice(3).filter(item => Number(item));
+
+!numbers.length
+  ? global.console.log(`There is nothing to ${operation}`)
+  : operation === 'add'
+    ? global.console.log(numbers.reduce((a, b) => a + b, 0))
+    : operation === 'multiply'
+      ? global.console.log(numbers.reduce((a, b) => 1 * b, 1))
+      : global.console.log(`${operation} is not supported`);
