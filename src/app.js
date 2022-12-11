a9d70d5c75ae89971fcc58f1127afa0d6bd588bc
Oleksandr Krynytskyi <alex.krynytskyi@gmail.com>
2022-12-11 16:28:28 +0200

    changed code styles

--- src/app.js
+++ src/app.js
@@ -1,27 +1,24 @@
 'use strict';
 
 const inputNumbers = process.argv.slice(3)
-  .filter((item) => !isNaN(+item)).map(item => +item);
+  .filter((item) => !isNaN(+item))
+  .map((item) => +item);
 
 const inputAction = process.argv[2];
 
 function calculations(action, numbers) {
-  if (numbers.length === 0) {
-
-    return 'There is nothing to ' + action;
+  if (!numbers.length) {
+    return `There is nothing to ${action}`;
   }
 
   switch (action) {
     case 'add':
-
       return numbers.reduce((a, b) => +a + +b);
 
     case 'multiply':
-
       return numbers.reduce((a, b) => +a * +b);
 
     default:
-      
       return 'Operation is not supported!';
   }
 }
