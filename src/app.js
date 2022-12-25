35be0572f7f6683283446c0a7fb934b78a8e4f33
Olga Vasylevskaya <olhavasylevska@vectoriam.com>
2022-12-25 21:10:56 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const operation = process.argv[2];
+
+if (operation !== 'add' && operation !== 'multiply') {
+  console.log('Operation is not supported!');
+
+  return;
+}
+
+const params = process.argv.slice(3);
+const numericParams = params.filter((a) => (
+  typeof parseInt(a) === 'number' && !isNaN(parseInt(a))
+));
+
+if (!numericParams.length) {
+  console.log(`There is nothing to ${operation}`);
+
+  return;
+}
+
+if (operation === 'add') {
+  console.log(numericParams.reduce((a, b) => parseInt(a) + parseInt(b)));
+} else if (operation === 'multiply') {
+  console.log(numericParams.reduce((a, b) => parseInt(a) * parseInt(b)));
+}
