c4bbcdf7e6579be45c0aed909da348c7fc067a95
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2023-01-11 20:19:23 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
+
+const [operation, ...values] = process.argv.slice(2);
+const numericValues = values
+  .filter(value => !isNaN(value))
+  .map(value => Number(value));
+
+switch (operation) {
+  case 'add': {
+    if (!numericValues.length) {
+      console.log(`There is nothing to ${operation}`);
+    }
+    console.log(numericValues.reduce((total, value) => total + value), 0);
+    break;
+  }
+
+  case 'multiply': {
+    if (!numericValues.length) {
+      console.log(`There is nothing to ${operation}`);
+    }
+    console.log(numericValues.reduce((total, value) => total * value, 1));
+    break;
+  }
+
+  default:
+    console.log('Operation is not supported!');
+}
