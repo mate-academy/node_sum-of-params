943af87ced6daf2c6e901f73bb14be3768f1b3da
Lefteris Loizides <loizides.lef@gmail.com>
2023-02-13 18:55:56 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const operation = process.argv[2];
+
+const operands = process.argv.slice(3)
+  .filter(n => !isNaN(Number(n)))
+  .map(n => Number(n));
+
+const result = {
+  add: operands.reduce((a, b) => a + b, 0),
+  multiply: operands.reduce((a, b) => a * b, 1),
+};
+
+let message = result[operation];
+
+switch (true) {
+  case !(operation in result):
+    message = 'Operation is not supported!';
+    break;
+
+  case !operands.length:
+    message = 'There is nothing to add';
+    break;
+
+  default: break;
+}
+
+// eslint-disable-next-line no-console
+console.log(message);
