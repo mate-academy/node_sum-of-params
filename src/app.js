dbac62021b48ac94b58dde2ac57810fcf40fac17
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-22 09:05:51 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const params = process.argv.slice(2);
+const operation = params[0];
+const numbers = params.slice(1).map(Number).filter(number => !isNaN(number));
+let result;
+
+if (operation !== 'add' && operation !== 'multiply') {
+  result = 'Operation is not supported!';
+} else if (numbers.length === 0) {
+  result = `There is nothing to ${operation}`;
+} else if (operation === 'add') {
+  result = numbers.slice((a, b) => a + b, 0);
+} else if (operation === 'multiply') {
+  result = numbers.slice((a, b) => a * b, 1);
+}
+
+// eslint-disable-next-line no-console
+console.log(result);
