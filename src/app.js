b1a0cfac2c50c2b37f569433dea9a4c3fc358674
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-23 11:48:41 +0100

    switch-case and reduce

--- src/app.js
+++ src/app.js
@@ -5,14 +5,24 @@ const operation = params[0];
 const numbers = params.slice(1).map(Number).filter(number => !isNaN(number));
 let result;
 
-if (operation !== 'add' && operation !== 'multiply') {
-  result = 'Operation is not supported!';
-} else if (numbers.length === 0) {
-  result = `There is nothing to ${operation}`;
-} else if (operation === 'add') {
-  result = numbers.slice((a, b) => a + b, 0);
-} else if (operation === 'multiply') {
-  result = numbers.slice((a, b) => a * b, 1);
+switch (operation) {
+  case 'add':
+    if (numbers.length === 0) {
+      result = 'There is nothing to add';
+    } else {
+      result = numbers.reduce((a, b) => a + b, 0);
+    }
+    break;
+  case 'multiply':
+    if (numbers.length === 0) {
+      result = 'There is nothing to multiply';
+    } else {
+      result = numbers.reduce((a, b) => a * b, 1);
+    }
+    break;
+  default:
+    result = 'Operation is not supported!';
+    break;
 }
 
 // eslint-disable-next-line no-console
