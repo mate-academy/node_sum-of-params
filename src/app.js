26d79b49d5a2dd8810ff7e650ea9ff98afc8562f
Saleh BUD <saleh.7734@gmail.com>
2023-03-01 20:12:25 +0200

    Complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const operation = process.argv[2];
+const params = process.argv.slice(3);
+
+const calculate = () => {
+  if (!params.length) {
+    return "There is nothing to add";
+  }
+
+  switch (operation) {
+    case "add":
+      return params.reduce((a, b) => Number(a) + Number(b));
+
+    case "multiply":
+      return params.reduce((a, b) => Number(a) * Number(b));
+
+    default:
+      return "Operation is not supported";
+  }
+};
+
+const output = String(calculate());
+
+process.stdout.write(output);
