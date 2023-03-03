3e2a6a3a67bf8ea3b101b29f9dd42a5a736c9b4b
Levente Krotos <krotos.levi@gmail.com>
2023-03-03 20:01:59 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
+/* eslint-disable no-console */
 'use strict';
+
+const operation = process.argv.slice(2)[0];
+
+const numbers = process.argv.slice(3).map(elem => Number(elem));
+
+if (!numbers.length) {
+  console.log(`There is nothing to ${operation}`);
+} else {
+  switch (operation) {
+    case 'add':
+      const resultAdd = numbers
+        .reduce((element, sum) => sum + parseInt(element));
+
+      console.log(resultAdd);
+      break;
+
+    case 'multiply':
+      const resultMultiply = numbers
+        .reduce((element, sum) => sum * Number(element));
+
+      console.log(resultMultiply);
+      break;
+
+    default:
+      console.log('Operation is not supported!');
+  }
+}
