312e6d8b5267caa11420b5cfcba095b06d7476aa
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-14 20:06:42 +0200

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
-'use strict';
+/* eslint-disable no-console */
+const params = process.argv.slice(2);
+
+const HandleParams = (paramsObj) => {
+  const operation = paramsObj[0];
+  const availableOperations = ['multiply', 'add'];
+
+
+  if (!availableOperations.includes(operation)) {
+    console.log('Operation is not supported!');
+
+    return '';
+  }
+
+  const numbers = paramsObj.slice(1).filter(num => {
+    if (!isNaN(num)) {
+      return num;
+    }
+  });
+
+  if (!numbers.length) {
+    console.log('There is nothing to ' + operation);
+
+    return '';
+  }
+
+  if (operation === 'add') {
+    const sum = numbers.reduce((a, b) => Number(a) + Number(b));
+
+    console.log(sum);
+  }
+
+  if (operation === 'multiply') {
+    const multiply = numbers.reduce((a, b) => Number(a) * Number(b), 1);
+
+    console.log(multiply);
+  }
+};
+
+HandleParams(params);
