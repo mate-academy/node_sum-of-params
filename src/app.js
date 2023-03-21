4b8ad3d83387f4693ec104dffdc317f4f9526576
Mohamed Dahi <mohamedjane362@gmail.com>
2023-03-21 12:27:56 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const parameters = process.argv.slice(2);
+
+// eslint-disable-next-line no-console
+console.log(calculate(parameters));
+
+function calculate(params) {
+  let result = 0;
+
+  switch (params[0]) {
+    case 'add':
+      if (params.length < 2) {
+        return `There is nothing to add`;
+      }
+
+      for (const element of params) {
+        if (element === 'add') {
+          continue;
+        }
+
+        result += Number(element);
+      }
+
+      return result;
+
+    case 'multiply':
+      result = 1;
+
+      if (params.length < 2) {
+        return `There is nothing to multiply`;
+      }
+
+      for (const element of params) {
+        if (element === 'multiply') {
+          continue;
+        }
+        result *= Number(element);
+      }
+
+      return result;
+
+    default:
+      return 'Operation is not supported!';
+  }
+}
