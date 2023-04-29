51d31b298651c7f0e42a0e11b04f9110eec81dff
Jānis Žurilo <zurilojohn@gmail.com>
2023-04-29 19:50:47 +0300

    Finished

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const operation = process.argv[2];
+const numbers = process.argv
+  .slice(3)
+  .map(Number)
+  .filter((value) => !isNaN(value));
+
+if (numbers.length === 0) {
+  console.log('There is nothing to add or multiply.');
+} else {
+  switch (operation) {
+    case 'add':
+      const sum = numbers.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      );
+
+      console.log(`The sum of all numbers is ${sum}`);
+      break;
+    case 'multiply':
+      const product = numbers.reduce(
+        (accumulator, currentValue) => accumulator * currentValue,
+        1
+      );
+
+      console.log(`The product of all numbers is ${product}`);
+      break;
+    default:
+      console.log('Operation is not supported!');
+  }
+}
