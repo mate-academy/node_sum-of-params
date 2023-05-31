282a8ece4396295039da87a4e0196ba1bc956763
Metin BICAKSIZ <metinbicaksiz@mail.com>
2023-05-31 14:47:01 +0300

    optimized

--- src/app.js
+++ src/app.js
@@ -7,25 +7,28 @@ const operation = args[0];
 
 const numbers = args.slice(1).filter(arg => !isNaN(arg)).map(Number);
 
-if (numbers.length === 0) {
-  if (operation === 'add') {
-    console.log('There is nothing to add.');
-  } else if (operation === 'multiply') {
-    console.log('There is nothing to multiply.');
-  } else {
-    console.log('Operation is not supported!');
+if (!numbers.length) {
+  switch (operation) {
+    case 'add' || 'multiply':
+      console.log(`There is nothing to ${operation}`);
+      break;
+    default:
+      console.log('Operation is not supported!');
+      process.exit(1);
   }
 } else {
   let result;
 
-  if (operation === 'add') {
-    result = numbers.reduce((acc, num) => acc + num, 0);
-  } else if (operation === 'multiply') {
-    result = numbers.reduce((product, num) => product * num, 1);
-  } else {
-    console.log('Operation is not supported!');
-    process.exit(1);
+  switch (operation) {
+    case 'add':
+      result = numbers.reduce((acc, num) => acc + num, 0);
+      break;
+    case 'multiply':
+      result = numbers.reduce((product, num) => product * num, 1);
+      break;
+    default:
+      console.log('Operation is not supported!');
+      process.exit(1);
   }
-
   console.log(result);
 }
