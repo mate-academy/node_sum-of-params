15a9ccdc102db9fd9a4cbc165d28bec26d8efaca
Oleksandr Kapustei <angelion541@gmail.com>
2023-08-23 14:27:34 +0300

    finish task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
+/* eslint-disable no-console */
 'use strict';
+
+const paramsFunction = process.argv.slice(2, 3)[0];
+const paramsValues = process.argv.slice(3).reduce((acc, total) => {
+  if (!isNaN(+total)) {
+    acc.push(+total);
+  }
+
+  return acc;
+}, []);
+
+if (paramsFunction === 'add') {
+  if (paramsValues.length === 0) {
+    console.log(`There is nothing to add`);
+  } else {
+    add(...paramsValues);
+  }
+} else if (paramsFunction === 'multiply') {
+  if (paramsValues.length === 0) {
+    console.log(`There is nothing to add`);
+  } else {
+    multiply(...paramsValues);
+  }
+} else {
+  console.log(`Operation is not supported!`);
+}
+
+function add(...args) {
+  let result = 0;
+
+  args.forEach((arg) => {
+    result += arg;
+  });
+
+  console.log(result);
+}
+
+function multiply(...args) {
+  let result = 1;
+
+  args.forEach((arg) => {
+    result *= arg;
+  });
+
+  console.log(result);
+}
