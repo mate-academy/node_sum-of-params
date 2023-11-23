2371a341e040ae03da7d5488f595441fc9de4951
efdalyalcin <efdalyalcin@hotmail.com>
2023-11-23 18:57:50 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const [operation, ...values] = process.argv.slice(2);
+
+const numeratedValues = values.map((value) => {
+  return Number(value);
+});
+
+const nanRemovedNumbers = numeratedValues.filter((value) => !isNaN(value));
+
+const add = (numberArray) => {
+  return numberArray.reduce((total, addition) => total + addition, 0);
+};
+
+const multiply = (numberArray) => {
+  return numberArray.reduce((total, addition) => total * addition, 1);
+};
+
+switch (operation) {
+  case 'add':
+    add(nanRemovedNumbers);
+    break;
+  case 'multiply':
+    multiply(nanRemovedNumbers);
+    break;
+  default:
+    return nanRemovedNumbers;
+}
